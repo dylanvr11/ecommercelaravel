0d7d7e4c23c3dde998ac65408b2496da03606d5b
dylanvr11 <dylanvr97@gmail.com>
2023-07-07 07:08:05 -0500

    se elimina la importacion exampel component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,13 +3,11 @@ import { createApp } from 'vue'
 import vSelect from 'vue-select'
 
 // Components --------------------------------------------------
-import ExampleComponent from './components/ExampleComponent.vue'
 import ProductsList from './components/Products/Index.vue'
 import CategoriesList from './components/Categories/Index.vue'
 import CartsList from './components/Carts/Index.vue'
 const app = createApp({
 	components: {
-		ExampleComponent,
 		ProductsList,
 		CategoriesList,
 		CartsList
