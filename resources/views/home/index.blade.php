d3ad0c3f0d5ea00f36eab9e6a361cf3f39394e8a
dylanvr11 <dylanvr97@gmail.com>
2023-07-06 22:38:27 -0500

    home foto

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,15 +5,18 @@
                 <h1>{{$category->name}}</h1>
             </div>
             @foreach ($category->Products as $product)
+            <a href="{{ route('product.get',['product' => $product->id]) }}">
                 <div class="card mx-3 my-3" style="width: 18rem;">
                     @if($product->image)
                         <img src="/storage/images/{{$product->image}}"         class="card-img-top" alt="Libro">
                     @else
                         <img src="https://previews.123rf.com/images/nosua/nosua1710/nosua171000361/88191655-colecci%C3%B3n-de-productos-electr%C3%B3nicos-de-consumo-que-vuelan-en-el-aire-3d-render-sobre-fondo-blanco.jpg"         class="card-img-top" alt="Libro">
                     @endif
+            </a>
                         <div class="card-body">
                             <h5 class="card-title">{{$product->name}}</h5>
                             <p class="card-text">{{$product->description}}</p>
+                            <p class="card-text">Stock: {{$product->stock}}</p>
                             <form action="{{route('add.cart.post',['product'=>$product->id])}}" method="post">
                                 @csrf
                                 <input type="number" name="quantity">
